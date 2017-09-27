57c398bb9cb9a9d33d414ab1d275ca40db10534a
Dominic Pain <dominic@flowmo.co>
2017-09-27 14:35:45 +0100

    add plugins as submodles

---
+++ vim/bundle/get-modules.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git submodule add git://github.com/vim-scripts/bufexplorer.zip bundle/bufexplorer
+git submodule add git://github.com/scrooloose/nerdcommenter.git bundle/nerdcommenter
+git submodule add git://github.com/scrooloose/nerdtree.git bundle/nerdtree
+git submodule add git://github.com/tpope/vim-surround.git bundle/surround
+git submodule add git://github.com/hail2u/vim-css3-syntax.git bundle/css3
+git submodule add git://github.com/Townk/vim-autoclose.git bundle/autoclose
