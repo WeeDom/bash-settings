68cfaee4b167a8e1d5d75badbc2b91996a0fde82
Dominic Pain <dominic.pain@simitive.com>
2021-05-20 17:38:18 +0100

    remove some mess, and re-add sobmodules correctly

--- vim/bundle/get-modules.sh
+++ vim/bundle/get-modules.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-git submodule add git://github.com/vim-scripts/bufexplorer.zip bundle/bufexplorer
-git submodule add git://github.com/scrooloose/nerdcommenter.git bundle/nerdcommenter
-git submodule add git://github.com/scrooloose/nerdtree.git bundle/nerdtree
-git submodule add git://github.com/tpope/vim-surround.git bundle/surround
-git submodule add git://github.com/hail2u/vim-css3-syntax.git bundle/css3
-git submodule add git://github.com/Townk/vim-autoclose.git bundle/autoclose
-git submodule add git://github.com/tpope/vim-fugitive.git bundle/fugitive
+git submodule add git://github.com/vim-scripts/bufexplorer.zip bufexplorer
+git submodule add git://github.com/scrooloose/nerdcommenter.git nerdcommenter
+git submodule add git://github.com/scrooloose/nerdtree.git nerdtree
+git submodule add git://github.com/tpope/vim-surround.git surround
+git submodule add git://github.com/hail2u/vim-css3-syntax.git css3
+git submodule add git://github.com/Townk/vim-autoclose.git autoclose
+git submodule add git://github.com/tpope/vim-fugitive.git fugitive
+
+
+git submodule init bufexplorer
+git submodule init nerdcommenter
+git submodule init nerdtree
+git submodule init surround
+git submodule init css3
+git submodule init autoclose
+git submodule init fugitive
