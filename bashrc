d03968a1e4439436f28395ef86317c0fef6f693d
Dominic Pain <dominic.pain@rabbies.com>
2025-03-26 16:10:33 +0000

    add docker sock

--- bashrc
+++ bashrc
@@ -70,3 +70,4 @@ alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export DOCKER_HOST=unix:///var/run/docker.sock
---
+++ vim/get-modules.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git submodule add https://github.com/scrooloose/nerdcommenter.git nerdcommenter
+git submodule add https://github.com/scrooloose/nerdtree.git nerdtree
+git submodule add https://github.com/tpope/vim-surround.git surround
+git submodule add https://github.com/hail2u/vim-css3-syntax.git css3
+git submodule add https://github.com/Townk/vim-autoclose.git autoclose
+git submodule add https://github.com/tpope/vim-fugitive.git fugitive
+
+
+git submodule init bufexplorer
+git submodule init nerdcommenter
+git submodule init nerdtree
+git submodule init surround
+git submodule init css3
+git submodule init autoclose
+git submodule init fugitive
