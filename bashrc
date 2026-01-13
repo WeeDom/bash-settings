7635ecbecfdeb05979f3af385a70f64342c49ee5
Dominic Pain <dominic_pain@hotmail.com>
2026-01-13 20:29:06 +0000

    update submodules

--- bashrc
+++ bashrc
@@ -183,3 +183,7 @@ alias psqlx='docker compose exec db psql -U scaffadmin -d scaffsmart -x'
 
 
 export PATH="/home/weedom/.local/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- vim/get-modules.sh
+++ vim/get-modules.sh
@@ -6,6 +6,7 @@ git submodule add https://github.com/tpope/vim-surround.git surround
 git submodule add https://github.com/hail2u/vim-css3-syntax.git css3
 git submodule add https://github.com/Townk/vim-autoclose.git autoclose
 git submodule add https://github.com/tpope/vim-fugitive.git fugitive
+git submodule add https://github.com/nvie/vim-flake8.git
 
 
 git submodule init bufexplorer
@@ -15,3 +16,4 @@ git submodule init surround
 git submodule init css3
 git submodule init autoclose
 git submodule init fugitive
+git submodule init flake8
